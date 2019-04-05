f78f13feb016ab307edb18982cb1310ad71a4512
jaemnkm <jaemnkm@gmail.com>
2019-04-05 11:27:05 -0700

    first push

---
+++ README.md
@@ -0,0 +1,11 @@
+# Das Das Digital 
+
+## FrontEnd
+
+### Home page
+### About page
+### Shop
+### Checkout
+### Admin/Portal
+### Your Invoices
+### Shop Packages
---
+++ code/frontend/js/main.js
@@ -0,0 +1,22 @@
+/* 
+        ########################################
+        ########################################
+        ######      Das Das Digital      #######
+        ######      Agency Web App       #######
+        ########################################
+        ########################################
+*/
+
+/* 
+
+    Redirect to home from index.
+
+*/
+if (window.location = "index.html"){
+    window.location.href = "app/home.html"
+}
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log("test");
+});
